7022a3e7b8073a4c3fb04fcc91164b40b349d07d
marcelstiefs <marcel.stiefs@web.de>
2024-06-06 16:33:28 +0200

    jump

--- js/classes/movable-objekt.class.js
+++ js/classes/movable-objekt.class.js
@@ -7,34 +7,49 @@ class MovableObject {
     imageCache = {};
     currentImage = 0;
     speed = 0.15;
-    otherDirection =false;
+    otherDirection = false;
+    speedY = 0;
+    accelaration = 2.5;
 
-loadImage(path){
-    this.img = new Image();
-    this.img.src = path;
-}
- loadImages(arr) {
-    arr.forEach((path) => {
-        let img = new Image();
-        img.src = path;
-        this.imageCache[path] = img;
-        
-    });
- }
-
- playAnimation(images){
-     let i = this.currentImage % this.IMAGES_WALKING.length;
-     let path = images[i];
-     this.img = this.imageCache[path];
-     this.currentImage++;
- }
-
-
-    moveRight(){
-        
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.accelaration;
+            }
+        }, 1000 / 25);
     }
 
-    moveLeft(){
+    isAboveGround() {
+        return this.y < 128;
+    }
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+
+        });
+    }
+
+    playAnimation(images) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
+
+    moveRight() {
+
+    }
+
+    moveLeft() {
         setInterval(() => {
             this.x -= this.speed;
         }, 1000 / 60);
