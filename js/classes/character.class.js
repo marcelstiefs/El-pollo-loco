f3c3465e3992be0ce4699f9ffce89d6f4c3a7091
marcelstiefs <marcel.stiefs@web.de>
2024-06-06 13:45:48 +0200

    walking sound

--- js/classes/character.class.js
+++ js/classes/character.class.js
@@ -12,7 +12,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-26.png',
     ];
     world;
-
+    walking_sound = new Audio('sounds/walking.mp3')
 
 
     constructor() {
@@ -23,14 +23,16 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
+            this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
-
+                this.walking_sound.play();
             }
             if (this.world.keyboard.LEFT && this.x > -400) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
             this.world.camera_x = -this.x + 100
         }, 1000 / 60);
