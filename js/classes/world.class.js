7e02f050e6eb100b111393529766e5b0dd8526dc
marcelstiefs <marcel.stiefs@web.de>
2024-06-06 12:02:44 +0200

    first push

---
+++ js/classes/world.class.js
@@ -0,0 +1,60 @@
+class World {
+    character = new Character();
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects
+    canvas;
+    ctx;
+    keyboard;
+    camera_x = 0;
+
+    constructor(canvas, keyboard) {
+        this.ctx = canvas.getContext("2d");
+        this.canvas = canvas;
+        this.keyboard = keyboard;
+        this.draw();
+        this.setWorld()
+    }
+
+    setWorld() {
+        this.character.world = this;
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.widt, this.canvas.height);
+
+        this.ctx.translate(this.camera_x, 0);
+
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.clouds);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+        this.ctx.translate(-this.camera_x, 0);
+
+        //draw() wird immer wieder aufgerufen
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(obj => {
+            this.addToMap(obj);
+        });
+    }
+
+    addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
+    }
+}
---
+++ levels/level1.js
@@ -0,0 +1,40 @@
+ const level1 = new Level(
+    [
+        new Chicken (),
+        new Chicken (),
+        new Chicken (),
+        new Chicken (),
+    ],
+    [
+        new Cloud()
+    ],
+     [
+         new BackgroundObject('img/5_background/layers/air.png', -719),
+         new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
+         new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
+         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
+
+
+         new BackgroundObject('img/5_background/layers/air.png', 1),
+         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 1),
+         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 1),
+         new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 1),
+
+         new BackgroundObject('img/5_background/layers/air.png', 719),
+         new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
+         new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
+         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+
+         new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
+         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
+         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
+         new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
+
+         new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
+         new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
+         new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
+         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
+
+
+     ]
+);
